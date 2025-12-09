c1e4c7db1073ff042ef67285db2b8775d92511eb
macbook <macbook@Kitans-Pro.local>
2025-12-09 12:42:44 +0100

    add: The home top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,44 @@
+import { Icon } from "@iconify/react";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="py-2 px-10">
+      <div className="w-full h-[80vh] relative">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/images/bg2.jpg"
+          alt="Background"
+          fill
+          className="rounded-3xl"
+          style={{ objectFit: "cover" }}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+
+        <div className="absolute inset-0 rounded-3xl backdrop-blur-[2px] "></div>
+
+        {/* Optional content on top */}
+        <div className="absolute inset-0 p-10 flex flex-col space-y-4 items-center justify-center md:items-start md:justify-start text-white">
+          <div className="space-y-4">
+            <h1 className="md:text-6xl text-3xl tracking-wider">Welcome</h1>
+            <h1 className="md:text-6xl pl-10 text-3xl tracking-wider">to</h1>
+            <h1 className="md:text-6xl text-3xl tracking-wider">Mofarms</h1>
+          </div>
+
+          <span className="hidden md:inline md:text-md w-1/2 text-sm font-medium tracking-wider">
+            Where enthusiasm meets your animal of choice.
+          </span>
+
+          <div className="mt-16 flex items-center">
+            Our Products
+            <Icon
+              icon="material-symbols-light:arrow-right"
+              width="25"
+              height="25"
+              className="cursor-pointer text-white hover:text-primary"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="text-foreground">
+      <div className="max-w-7xl text-sm mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          {/* Left */}
+          <div className="flex space-x-6">
+            <Link href="/" className="hover:text-primary">
+              Home
+            </Link>
+            <Link href="#" className="hover:text-primary">
+              Products
+            </Link>
+          </div>
+
+          {/* Center */}
+          <div className="shrink-0">
+            <Image
+              src={"/images/md farms.png"}
+              alt={""}
+              width={50}
+              height={50}
+            />
+          </div>
+
+          {/* Right */}
+          <div className="hidden md:flex space-x-4">
+            <Icon
+              icon="ic:round-phone"
+              width="25"
+              height="25"
+              className="cursor-pointer text-gray-500 hover:text-primary"
+            />
+            <Icon
+              icon="ic:sharp-whatsapp"
+              width="25"
+              height="25"
+              className="cursor-pointer text-gray-500 hover:text-primary"
+            />
+            <Icon
+              icon="material-symbols:mail"
+              width="25"
+              height="25"
+              className="cursor-pointer text-gray-500 hover:text-primary"
+            />
+          </div>
+
+          {/* Mobile menu button */}
+          <div className="md:hidden flex items-center">
+            <button
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="text-foreground focus:outline-none"
+            >
+              <Icon
+              className="text-gray-500 hover:text-primary"
+                icon={menuOpen ? "mdi:close" : "mdi:menu"}
+                width="28"
+                height="28"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu */}
+      {menuOpen && (
+        <div className="shadow-lg pb-8">
+          <div className="md:hidden px-2 pt-2 pb-3 space-y-1 bg-background">
+            <Link
+              href="#"
+              className="block px-3 py-2 rounded-md hover:bg-primary hover:text-white"
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              className="block px-3 py-2 rounded-md hover:bg-primary hover:text-white"
+            >
+              Products
+            </Link>
+          </div>
+          <div className="md:hidden flex justify-center space-x-6">
+            <Icon
+              icon="ic:round-phone"
+              width="25"
+              height="25"
+              className="cursor-pointer text-gray-500 hover:text-primary"
+            />
+            <Icon
+              icon="ic:sharp-whatsapp"
+              width="25"
+              height="25"
+              className="cursor-pointer text-gray-500 hover:text-primary"
+            />
+            <Icon
+              icon="material-symbols:mail"
+              width="25"
+              height="25"
+              className="cursor-pointer text-gray-500 hover:text-primary"
+            />
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+}
