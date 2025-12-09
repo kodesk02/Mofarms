8d21d1963fa640eaf47a03feb2086fa0851c1ab3
macbook <macbook@Kitans-Pro.local>
2025-12-09 16:25:27 +0100

    add: free

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,46 +1,3 @@
-// "use client";
-
-// import { useState } from "react";
-// import HoverIcon from "@/components/HoverIcon";
-// import ServiceCard from "@/components/ServiceCard";
-// import { SERVICE, SERVICE_CARDS } from "../data";
-
-// export default function ProductsPage() {
-//   const [activeService, setActiveService] = useState(SERVICE[0].id);
-
-//   const cards = SERVICE_CARDS[activeService as keyof typeof SERVICE_CARDS] || [];
-
-//   return (
-//     <div className="py-2 px-4 md:px-10">
-//         <div className="flex justify-center overflow-auto gap-6 mb-10">
-//           {SERVICE.map((service, index) => (
-//             <div
-//               key={index}
-//               onClick={() => setActiveService(service.id)}
-//             >
-//               <HoverIcon
-//                 icon={service.icon}
-//                 title={service.title}
-//                 isActive={activeService === service.id}           />
-//             </div>
-//           ))}
-//       </div>
-
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-//         {cards.map((card, index) => (
-//           <ServiceCard
-//             key={index}
-//             title={card.title}
-//             image={card.image}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-
-// app/products/page.tsx
 "use client";
 
 import { useState, useEffect } from "react";
@@ -86,7 +43,7 @@ export default function ProductsPage() {
         </div>
 
       {/* Cards Section */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {cards.map((card, index) => (
           <ServiceCard
             key={index}
