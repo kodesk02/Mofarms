51083f5b832f112fb9abafbbb5c171fa28b8db3a
macbook <macbook@Kitans-Pro.local>
2026-02-16 12:18:29 +0100

    fix

--- src/app/data.ts
+++ src/app/data.ts
@@ -21,170 +21,96 @@ export const SERVICE_CARDS = {
       title: "Peacocks",
       image: "/images/peacock.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/peacock1.jpg",
+        "/images/peacock2.jpg",
+        "/images/peacock3.jpg",
+        "/images/peacock4.jpg",
+        "/images/peacock5.jpg",
       ],
     },
     {
       title: "Ostrich",
       image: "/images/ostrich.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/ostrich1.jpg",
+        "/images/ostrich2.jpg",
+        "/images/ostrich3.jpg",
       ],
     },
     {
       title: "Foreign Chicken",
       image: "/images/foreign_chicken.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/chicken1.jpg",
+        "/images/chicken2.jpg",
+        "/images/chicken3.jpg",
+        "/images/chicken4.jpg",
+        "/images/chicken5.jpg",
       ],
     },
     {
       title: "Parrot",
       image: "/images/parrot.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/parrot1.jpg",
+        "/images/parrot2.jpg",
+        "/images/parrot3.jpg",
+        "/images/parrot4.jpg",
+        "/images/parrot5.jpg",
+        "/images/parrot6.jpg",
       ],
     },
-    {
-      title: "Duck",
-      image: "/images/duck.jpg",
-      gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
+    // {
+    //   title: "Duck",
+    //   image: "/images/duck.jpg",
+    //   gallery: [
+    //     "/images/peacock.jpg",
+    //     "/images/ostrich.jpg",
+    //     "/images/foreign_chicken.jpg",
 
-        "/images/foreign_chicken.jpg",
+    //     "/images/foreign_chicken.jpg",
 
-        "/images/foreign_chicken.jpg",
+    //     "/images/foreign_chicken.jpg",
 
-        "/images/foreign_chicken.jpg",
+    //     "/images/foreign_chicken.jpg",
 
-        "/images/foreign_chicken.jpg",
+    //     "/images/foreign_chicken.jpg",
 
-        "/images/foreign_chicken.jpg",
+    //     "/images/foreign_chicken.jpg",
 
-        "/images/foreign_chicken.jpg",
-      ],
-    },
+    //     "/images/foreign_chicken.jpg",
+    //   ],
+    // },
   ],
   farm: [
     {
       title: "Chicken",
       image: "/images/chicken.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/live1.jpg",
+        "/images/live2.jpg",
+        "/images/live3.jpg",
+        "/images/live4.jpg",
       ],
     },
     {
       title: "Goose",
       image: "/images/geese.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/geese1.jpg",
+        "/images/geese2.jpg",
+        "/images/geese3.jpg",
+        "/images/geese4.jpg",
       ],
     },
     {
       title: "Goat",
       image: "/images/goat.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/goat1.jpg",
+        "/images/goat2.jpg",
+        "/images/goat3.jpg",
+        "/images/goat4.jpg",
       ],
     },
   ],
@@ -193,42 +119,18 @@ export const SERVICE_CARDS = {
       title: "Rabbit",
       image: "/images/rabbit.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/rabbit1.jpg",
+        "/images/rabbit2.jpg",
+        "/images/rabbit3.jpg",
       ],
     },
     {
       title: "Guinea Pig",
       image: "/images/hamster.jpg",
       gallery: [
-        "/images/peacock.jpg",
-        "/images/ostrich.jpg",
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
-
-        "/images/foreign_chicken.jpg",
+        "/images/pig1.jpg",
+        "/images/pig2.jpg",
+        "/images/pig3.jpg",
       ],
     },
   ],
